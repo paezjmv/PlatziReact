a2fec3c2ee036eeb5c45ae91eb2e6c63710f56af
paezjmv <paezjm86@gmail.com>
2021-02-02 19:31:31 -0600

    Adding Styles to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import '../assets/styles/components/Header.scss';
 
 const Header = () => (
   <header className='header'>
     <img className='header__img' src='assets/logo-platzi-video-BW2.png' alt='Platzi Video' />
     <div className='header__menu'>
       <div className='header__menu--profile'>
-        <img src='assets/icon-platzivideo.png' alt='User' />
+        <img src='assets/icon-platzivideo.png' alt='' />
         <p>Perfil</p>
       </div>
       <ul>
