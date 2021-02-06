0d1679b95927d95e3452940b4e8e407818f38bfd
paezjmv <paezjm86@gmail.com>
2021-02-05 20:04:21 -0600

    Fixing Styles on carrousel item detail

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -8,8 +8,8 @@ const CarouselItem = () => (
     <img className='carrousel-item__img' src='https://images.pexels.com/photos/1181676/pexels-photo-1181676.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='Woman view' />
     <div className='carrousel-item__details'>
       <div>
-        <img className='carousel-item__details--img' src={playIcon} alt='Play' />
-        <img className='carousel-item__details--img' src={plusIcon} alt='Plus' />
+        <img className='carrousel-item__details--img' src={playIcon} alt='Play' />
+        <img className='carrousel-item__details--img' src={plusIcon} alt='Plus' />
       </div>
       <p className='carrousel-item__details--title'>Titulo descriptivo</p>
       <p className='carrousel-item__details--subtitle'>2020 16+ 114</p>
