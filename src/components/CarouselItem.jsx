9ba692f76178e527b99ad94efdd3da7a28d4d506
paezjmv <paezjm86@gmail.com>
2021-02-09 19:02:01 -0600

    Adding a fake API for React Hooks

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -3,16 +3,18 @@ import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/icon-play.png';
 import plusIcon from '../assets/static/icon-plus.png';
 
-const CarouselItem = () => (
+const CarouselItem = ({ cover, title, year, contentRaiting, duration }) => (
   <div className='carrousel-item'>
-    <img className='carrousel-item__img' src='https://images.pexels.com/photos/1181676/pexels-photo-1181676.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500' alt='Woman view' />
+    <img className='carrousel-item__img' src={cover} alt={title} />
     <div className='carrousel-item__details'>
       <div>
         <img className='carrousel-item__details--img' src={playIcon} alt='Play' />
         <img className='carrousel-item__details--img' src={plusIcon} alt='Plus' />
       </div>
-      <p className='carrousel-item__details--title'>Titulo descriptivo</p>
-      <p className='carrousel-item__details--subtitle'>2020 16+ 114</p>
+      <p className='carrousel-item__details--title'>{title}</p>
+      <p className='carrousel-item__details--subtitle'>
+        {`${year} ${contentRaiting} ${duration}`}
+      </p>
     </div>
   </div>
 );
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
@@ -7,35 +7,44 @@ import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
 import '../assets/styles/App.scss';
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <Search />
+const App = () => {
+  const [videos, setVideos] = useState({ mylist: [], trends: [], originals: [] });
 
-    <Categories title='Mi lista'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  useEffect(() => {
+    fetch('http://localhost:3000/initialState')
+      .then(response => response.json())
+      .then(data => setVideos(data));
+  }, []);
 
-    <Categories title='Tendencias'>
-      <Carousel>
-        <CarouselItem />
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+  return (
+    <div className='App'>
+      <Header />
+      <Search />
 
-    <Categories title='Originales de PlatziVideo'>
-      <Carousel>
-        <CarouselItem />
-      </Carousel>
-    </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title='Mi lista'>
+          <Carousel>
+            <CarouselItem />
+          </Carousel>
+        </Categories>
+      )}
 
-    <Footer />
-  </div>
-);
+      <Categories title='Tendencias'>
+        <Carousel>
+          {videos.trends.map(item =>
+            <CarouselItem key={item.id} {...item} />) }
+        </Carousel>
+      </Categories>
 
+      <Categories title='Originales de PlatziVideo'>
+        <Carousel>
+          {videos.originals.map(item =>
+            <CarouselItem key={item.id} {...item} />)}
+        </Carousel>
+      </Categories>
+
+      <Footer />
+    </div>
+  );
+};
 export default App;
