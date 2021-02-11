9331b9da3280cd56ee51166af33c7ef54de89476
paezjmv <paezjm86@gmail.com>
2021-02-10 20:42:07 -0600

    Adding Route

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -10,7 +10,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initialState';
 
-const App = () => {
+const Home = () => {
 
   const initialState = useInitialState(API);
 
@@ -46,4 +46,4 @@ const App = () => {
     </div>
   );
 };
-export default App;
+export default Home;
