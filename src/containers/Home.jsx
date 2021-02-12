0314ce66895a01bf120cfddedf8e5e4954aa13b8
paezjmv <paezjm86@gmail.com>
2021-02-12 16:48:41 -0600

    Adding Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import Header from '../components/Header';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import Footer from '../components/Footer';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
@@ -15,8 +13,7 @@ const Home = () => {
   const initialState = useInitialState(API);
 
   return initialState.length === 0 ? <h1> Loading... </h1> : (
-    <div className='App'>
-      <Header />
+    <>
       <Search />
 
       {initialState.mylist.length > 0 && (
@@ -42,8 +39,8 @@ const Home = () => {
         </Carousel>
       </Categories>
 
-      <Footer />
-    </div>
+    </>
   );
 };
+
 export default Home;
