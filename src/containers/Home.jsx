aaef6e9f688ae8d3b314491d987589385b0b59a2
paezjmv <paezjm86@gmail.com>
2021-02-15 21:39:06 -0600

    Fixing data of ReduxStore

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -4,7 +4,6 @@ import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
-import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 const Home = ({ myList, trends, originals }) => {
@@ -39,7 +38,7 @@ const Home = ({ myList, trends, originals }) => {
   );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     myList: state.myList,
     trends: state.trends,
