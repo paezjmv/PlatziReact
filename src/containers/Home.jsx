1d2493da3b128301c79163f83b6ef56ac9936ca5
paezjmv <paezjm86@gmail.com>
2021-02-15 19:17:17 -0600

    Creating Redux Store

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Search from '../components/Search';
 import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
@@ -6,20 +7,15 @@ import CarouselItem from '../components/CarouselItem';
 import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
-const API = 'http://localhost:3000/initialState';
-
-const Home = () => {
-
-  const initialState = useInitialState(API);
-
-  return initialState.length === 0 ? <h1> Loading... </h1> : (
+const Home = ({ myList, trends, originals }) => {
+  return (
     <>
       <Search />
 
-      {initialState.mylist.length > 0 && (
+      {myList.length > 0 && (
         <Categories title='Mi lista'>
           <Carousel>
-            {initialState.mylist.map((item) =>
+            {myList.map((item) =>
               <CarouselItem key={item.id} {...item} />) }
           </Carousel>
         </Categories>
@@ -27,14 +23,14 @@ const Home = () => {
 
       <Categories title='Tendencias'>
         <Carousel>
-          {initialState.trends.map((item) =>
+          {trends.map((item) =>
             <CarouselItem key={item.id} {...item} />) }
         </Carousel>
       </Categories>
 
       <Categories title='Originales de PlatziVideo'>
         <Carousel>
-          {initialState.originals.map((item) =>
+          {originals.map((item) =>
             <CarouselItem key={item.id} {...item} />)}
         </Carousel>
       </Categories>
@@ -43,4 +39,12 @@ const Home = () => {
   );
 };
 
-export default Home;
+const mapStateToProps = state => {
+  return {
+    myList: state.myList,
+    trends: state.trends,
+    originals: state.originals,
+  };
+};
+
+export default connect(mapStateToProps, null)(Home);
