7a485c1b8d9820b177e2e84bd00654cbcc9dc1ee
paezjmv <paezjm86@gmail.com>
2021-02-16 19:13:14 -0600

    Adding myList action and reducers

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { setFavorite } from '../actions/index';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/icon-play.png';
 import plusIcon from '../assets/static/icon-plus.png';
 
-const CarouselItem = ({ cover, title, year, contentRaiting, duration }) => (
-  <div className='carrousel-item'>
-    <img className='carrousel-item__img' src={cover} alt={title} />
-    <div className='carrousel-item__details'>
-      <div>
-        <img className='carrousel-item__details--img' src={playIcon} alt='Play' />
-        <img className='carrousel-item__details--img' src={plusIcon} alt='Plus' />
+const CarouselItem = (props) => {
+  const { cover, title, year, contentRaiting, duration } = props;
+  const handleSetFavorite = () => {
+    props.setFavorite({
+      cover, title, year, contentRaiting, duration,
+    });
+  };
+
+  return (
+    <div className='carrousel-item'>
+      <img className='carrousel-item__img' src={cover} alt={title} />
+      <div className='carrousel-item__details'>
+        <div>
+          <img className='carrousel-item__details--img' src={playIcon} alt='Play' />
+          <img className='carrousel-item__details--img' src={plusIcon} alt='Plus' onClick={handleSetFavorite} />
+        </div>
+        <p className='carrousel-item__details--title'>{title}</p>
+        <p className='carrousel-item__details--subtitle'>
+          {`${year} ${contentRaiting} ${duration}`}
+        </p>
       </div>
-      <p className='carrousel-item__details--title'>{title}</p>
-      <p className='carrousel-item__details--subtitle'>
-        {`${year} ${contentRaiting} ${duration}`}
-      </p>
     </div>
-  </div>
-);
+  );
+};
 
 CarouselItem.propTypes = {
   cover: PropTypes.string,
@@ -28,4 +39,8 @@ CarouselItem.propTypes = {
   duration: PropTypes.number,
 };
 
-export default CarouselItem;
+const mapDispatchToProps = {
+  setFavorite,
+};
+
+export default connect(null, mapDispatchToProps)(CarouselItem);
